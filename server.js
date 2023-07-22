ccf36d096802210a42cd2158f205799f87ebcf81
Fabian Ramirez <framirezvidal@gmail.com>
2023-07-21 23:48:01 -0400

    Setup V2

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+//Configure ENV
+dotenv.config();
+
+//database conig
+connectDB();
 
 //rest object
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
 
 //rest api
 app.get('/', (req,res) => {
@@ -10,9 +23,9 @@ app.get('/', (req,res) => {
 });
 
 //PORT
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`.bgCyan.white);
+    console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
 });
